2546c36e06ee7afafd0aa08647c01f46be6b523c
codongmin <codongmin@gmail.com>
2025-02-17 23:53:33 +0900

    [#6] Test: 유저 생성 실패 테스트 수정 - 예외 메시지 검증

--- src/test/groovy/com/devtribe/devtribe_feed_service/user/application/UserServiceTest.groovy
+++ src/test/groovy/com/devtribe/devtribe_feed_service/user/application/UserServiceTest.groovy
@@ -41,49 +41,59 @@ class UserServiceTest extends Specification {
     def "유효하지 않은 이메일로 유저 생성 요청할 경우 회원가입에 실패한다."() {
         given:
         def request = Mock(CreateUserRequest)
-        emailValidator.validateEmail(request.email()) >> { throw new IllegalArgumentException() }
+        emailValidator.validateEmail(request.email()) >> { throw new IllegalArgumentException(message) }
 
         when:
         userService.createUser(request)
 
         then:
-        thrown(IllegalArgumentException)
+        def e = thrown(IllegalArgumentException)
+        e.getMessage() == message
+
+        where:
+        message << ["이메일은 비어 있거나 null일 수 없습니다.", "유효하지 않은 이메일 형식입니다.", "이메일에 허용되지 않는 문자가 포함되어 있습니다."]
     }
 
     def "유효하지 않은 비밀번호로 유저 생성 요청할 경우 회원가입에 실패한다."() {
         given:
         def request = Mock(CreateUserRequest)
-        passwordValidator.validatePassword(request.password()) >> { throw new IllegalArgumentException() }
+        passwordValidator.validatePassword(request.password()) >> { throw new IllegalArgumentException(message) }
 
         when:
         userService.createUser(request)
 
         then:
-        thrown(IllegalArgumentException)
+        def e = thrown(IllegalArgumentException)
+        e.getMessage() == message
+
+        where:
+        message << ["비밀번호는 빈 값일 수 없습니다.", "비밀번호의 길이가 유효하지 않습니다.", "비밀번호는 대문자, 소문자, 숫자 및 특수문자를 모두 포함해야 합니다."]
     }
 
     def "유효하지 않은 자기소개로 유저 생성 요청할 경우 회원가입에 실패한다."() {
         given:
         def request = Mock(CreateUserRequest)
-        createUserRequestValidator.validateBiography(request.biography()) >> { throw new IllegalArgumentException() }
+        createUserRequestValidator.validateBiography(request.biography()) >> { throw new IllegalArgumentException(message) }
 
         when:
         userService.createUser(request)
 
         then:
-        thrown(IllegalArgumentException)
+        def e = thrown(IllegalArgumentException)
+        e.getMessage() == message
     }
 
     def "이미 존재하는 이메일로 유저 생성 요청할 경우 회원가입에 실패한다."() {
         given:
         def request = Mock(CreateUserRequest)
-        userRepository.isEmailRegistered(request.biography()) >> true
+        userRepository.isEmailRegistered(request.email()) >> true
 
         when:
         userService.createUser(request)
 
         then:
-        thrown(IllegalArgumentException)
+        def e = thrown(IllegalArgumentException)
+        e.getMessage() == "이미 가입된 이메일입니다."
     }
 
     def "이미 존재하는 닉네임으로 유저 생성 요청할 경우 회원가입에 실패한다."() {
@@ -95,6 +105,7 @@ class UserServiceTest extends Specification {
         userService.createUser(request)
 
         then:
-        thrown(IllegalArgumentException)
+        def e = thrown(IllegalArgumentException)
+        e.getMessage() == "이미 사용중인 닉네임입니다."
     }
 }
