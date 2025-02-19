77ae93542984f00a06036e2b1390319aad2bb637
codongmin <codongmin@gmail.com>
2025-02-19 18:21:52 +0900

    [#8] Refactor: 자기소개 검증 로직에서 null 체크 개선
    - biography가 null인 경우 즉시 반환하도록 로직 수정
    - 불필요한 조건문 중첩을 제거하여 코드 가독성 향상

--- src/main/java/com/devtribe/devtribe_feed_service/user/application/validators/CreateUserRequestValidator.java
+++ src/main/java/com/devtribe/devtribe_feed_service/user/application/validators/CreateUserRequestValidator.java
@@ -5,7 +5,11 @@ public class CreateUserRequestValidator {
     public static final Integer MAX_BIOGRAPHY_LENGTH = 100;
 
     public void validateBiography(String biography) {
-        if (biography != null && biography.length() > MAX_BIOGRAPHY_LENGTH) {
+        if (biography == null) {
+            return;
+        }
+        
+        if (biography.length() > MAX_BIOGRAPHY_LENGTH) {
             throw new IllegalArgumentException("자기소개는 " + MAX_BIOGRAPHY_LENGTH + "자를 초과할 수 없습니다.");
         }
     }
