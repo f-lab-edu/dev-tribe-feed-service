f28fc22b85c48a1d682e55b746c32b730f284168
codongmin <codongmin@gmail.com>
2025-02-24 21:30:53 +0900

    [#5] Feat: Post 공개여부 추가

--- src/main/java/com/devtribe/devtribe_feed_service/post/domain/Post.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/domain/Post.java
@@ -2,20 +2,31 @@ package com.devtribe.devtribe_feed_service.post.domain;
 
 import com.devtribe.devtribe_feed_service.user.domain.User;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class Post {
 
     private Long id;
     private String title;
     private String content;
     private User author;
+    private ContentSource contentSource;
     private String thumbnail;
+    private Publication publication;
+    private Integer upvoteCount;
+    private Integer downvoteCount;
 
     @Builder
-    public Post(String title, String body, User author, String thumbnail) {
+    public Post(String title, String content, User author, ContentSource contentSource,
+        String thumbnail) {
         this.title = title;
-        this.content = body;
+        this.content = content;
         this.author = author;
         this.thumbnail = thumbnail;
+        this.contentSource = contentSource;
+        this.publication = Publication.PUBLIC;
+        this.upvoteCount = 0;
+        this.downvoteCount = 0;
     }
 }
