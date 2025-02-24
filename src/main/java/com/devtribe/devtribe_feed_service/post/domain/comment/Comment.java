4c7f9cbfb0c73dec8e4dd9e3ecfb68d1ac1b8e00
codongmin <codongmin@gmail.com>
2025-02-24 21:28:35 +0900

    [#5] Feat: Comment 도메인 추가

---
+++ src/main/java/com/devtribe/devtribe_feed_service/post/domain/comment/Comment.java
@@ -0,0 +1,26 @@
+package com.devtribe.devtribe_feed_service.post.domain.comment;
+
+import com.devtribe.devtribe_feed_service.post.domain.Post;
+import com.devtribe.devtribe_feed_service.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Comment {
+
+    private Long id;
+    private Post post;
+    private User user;
+    private String content;
+    private Integer upvoteCount;
+    private Integer downvoteCount;
+
+    @Builder
+    public Comment(Post post, User user, String content) {
+        this.post = post;
+        this.user = user;
+        this.content = content;
+        this.upvoteCount = 0;
+        this.downvoteCount = 0;
+    }
+}
