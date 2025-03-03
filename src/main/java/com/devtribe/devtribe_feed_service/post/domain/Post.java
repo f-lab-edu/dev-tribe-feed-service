570aa97caa31f4645fb883f2251638e3845293b1
codongmin <codongmin@gmail.com>
2025-03-03 15:05:07 +0900

    [#5] Feat: ContentSource 삭제

--- src/main/java/com/devtribe/devtribe_feed_service/post/domain/Post.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/domain/Post.java
@@ -14,20 +14,18 @@ public class Post {
     private String title;
     private String content;
     private User author;
-    private ContentSource contentSource;
     private String thumbnail;
     private Publication publication;
     private Integer upvoteCount;
     private Integer downvoteCount;
 
     @Builder
-    public Post(String title, String content, User author, ContentSource contentSource,
+    public Post(String title, String content, User author,
         String thumbnail) {
         this.title = title;
         this.content = content;
         this.author = author;
         this.thumbnail = thumbnail;
-        this.contentSource = contentSource;
         this.publication = Publication.PUBLIC;
         this.upvoteCount = 0;
         this.downvoteCount = 0;
