abf3e0b71c4e74c48ef25ce650b2ce3f858e0634
codongmin <codongmin@gmail.com>
2025-03-04 16:30:45 +0900

    [#5] Feat: Comment 엔티티 및 어노테이션 설정 추가

--- src/main/java/com/devtribe/devtribe_feed_service/post/domain/comment/Comment.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/domain/comment/Comment.java
@@ -1,27 +1,56 @@
 package com.devtribe.devtribe_feed_service.post.domain.comment;
 
-import com.devtribe.devtribe_feed_service.post.domain.Post;
-import com.devtribe.devtribe_feed_service.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
+@Entity
+@Table(name = "comment")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Post post;
-    private User user;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "content", nullable = false)
     private String content;
+
+    @Column(name = "upvote_count", nullable = false)
     private Integer upvoteCount;
+
+    @Column(name = "downvote_count", nullable = false)
     private Integer downvoteCount;
 
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     @Builder
-    public Comment(Post post, User user, String content) {
-        this.post = post;
-        this.user = user;
+    public Comment(Long postId, Long userId, String content) {
+        this.postId = postId;
+        this.userId = userId;
         this.content = content;
         this.upvoteCount = 0;
         this.downvoteCount = 0;
