3d883dcfdbf2c79c42e85b8b57e4104b3d8e4a47
codongmin <codongmin@gmail.com>
2025-03-07 16:40:02 +0900

    [#15] Feat: FeedController 추가

---
+++ src/main/java/com/devtribe/devtribe_feed_service/post/api/FeedController.java
@@ -0,0 +1,30 @@
+package com.devtribe.devtribe_feed_service.post.api;
+
+import com.devtribe.devtribe_feed_service.global.common.CursorPagination;
+import com.devtribe.devtribe_feed_service.post.application.FeedService;
+import com.devtribe.devtribe_feed_service.post.application.dtos.GetFeedResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/feeds")
+public class FeedController {
+
+    private final FeedService feedService;
+
+    public FeedController(FeedService feedService) {
+        this.feedService = feedService;
+    }
+
+    @GetMapping
+    public ResponseEntity<GetFeedResponse> getFeedListBySortOption(
+        @RequestParam(required = false) CursorPagination cursorPagination,
+        @RequestParam(required = false) String sort
+    ) {
+        GetFeedResponse response = feedService.getFeedListBySortOption(cursorPagination, sort);
+        return ResponseEntity.ok(response);
+    }
+}
