bf3339f661a7e2e7ba7040215bebe74cb0a76528
codongmin <codongmin@gmail.com>
2025-03-07 13:01:49 +0900

    [#15] Feat: 커서 기반 페이징을 위한 공통 DTO 클래스 추가
    - PageResponse, CursorPagination, CursorMetadata 클래스 생성. 커서 기반 페이징 처리에 필요한 데이터 구조 추가
    
    Refactor: cusorpagination Page siza 검증

---
+++ src/main/java/com/devtribe/devtribe_feed_service/global/common/CursorPagination.java
@@ -0,0 +1,16 @@
+package com.devtribe.devtribe_feed_service.global.common;
+
+public record CursorPagination(Long lastFetchedId, int pageSize) {
+
+    private static final int DEFAULT_PAG_SIZE = 10;
+    private static final int MAX_PAG_SIZE = 30;
+    private static final int MIN_PAG_SIZE = 1;
+
+    public static CursorPagination defaultCursorPagination() {
+        return new CursorPagination(null, DEFAULT_PAG_SIZE);
+    }
+
+    public boolean isPageSizeInRange() {
+        return this.pageSize <= MAX_PAG_SIZE && this.pageSize >= MIN_PAG_SIZE;
+    }
+}
