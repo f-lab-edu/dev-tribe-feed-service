c85796ad6255aa0f68b314149ac8c5cdf2f4d9b8
codongmin <codongmin@gmail.com>
2025-03-07 16:55:11 +0900

    [#12] Feat: PostRepositoryImpl 클래스 추가 및 JPA 레포지토리 연동

---
+++ src/main/java/com/devtribe/devtribe_feed_service/post/repository/PostRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.devtribe.devtribe_feed_service.post.repository;
+
+import com.devtribe.devtribe_feed_service.post.application.interfaces.PostRepository;
+import com.devtribe.devtribe_feed_service.post.domain.Post;
+import com.devtribe.devtribe_feed_service.post.repository.jpa.JpaPostRepository;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository {
+
+    private final JpaPostRepository postRepository;
+
+    public PostRepositoryImpl(JpaPostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Optional<Post> findById(Long postId) {
+        return postRepository.findById(postId);
+    }
+
+    @Override
+    public void delete(Post post) {
+        postRepository.delete(post);
+    }
+}
