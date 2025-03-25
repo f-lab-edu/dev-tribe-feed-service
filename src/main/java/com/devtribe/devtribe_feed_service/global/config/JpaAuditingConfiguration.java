1a651a902225eb15ae755f35fc2468e83025188f
codongmin <codongmin@gmail.com>
2025-03-25 16:58:22 +0900

    [#21] Feat: AuditorAware 설정 추가

--- src/main/java/com/devtribe/devtribe_feed_service/global/config/JpaAuditingConfiguration.java
+++ src/main/java/com/devtribe/devtribe_feed_service/global/config/JpaAuditingConfiguration.java
@@ -1,10 +1,16 @@
 package com.devtribe.devtribe_feed_service.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaAuditingConfiguration {
 
+    @Bean
+    public AuditorAware<Long> auditorAware() {
+        return new SpringSecurityAuditorAware();
+    }
 }
