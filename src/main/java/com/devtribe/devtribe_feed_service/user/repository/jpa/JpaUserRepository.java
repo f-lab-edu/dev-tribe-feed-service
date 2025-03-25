0cd8c6fbab8b4408fdd69a7bc7ec5e8f22e00125
codongmin <codongmin@gmail.com>
2025-03-25 13:27:40 +0900

    [#17] Feat: 회원가입시 PasswordEncoder를 활용한 비밀번호 암호화 추가

--- src/main/java/com/devtribe/devtribe_feed_service/user/repository/jpa/JpaUserRepository.java
+++ src/main/java/com/devtribe/devtribe_feed_service/user/repository/jpa/JpaUserRepository.java
@@ -1,6 +1,7 @@
 package com.devtribe.devtribe_feed_service.user.repository.jpa;
 
 import com.devtribe.devtribe_feed_service.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaUserRepository extends JpaRepository<User, Long> {
@@ -8,4 +9,6 @@ public interface JpaUserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByNickname(String nickname);
+
+    Optional<User> findByEmail(String email);
 }
