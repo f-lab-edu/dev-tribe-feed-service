021261ce45bce15d1190e0400bba7f83650fc9d7
codongmin <codongmin@gmail.com>
2025-03-28 14:42:29 +0900

    [#20] Feat: updateBy 수정일시 NotNull 옵션 제거

--- src/main/resources/db/migration/V3__add_created_by_and_updated_by.sql
+++ src/main/resources/db/migration/V3__add_created_by_and_updated_by.sql
@@ -1,29 +1,29 @@
 ALTER TABLE user
     ADD COLUMN created_by BIGINT NOT NULL,
-    ADD COLUMN updated_by BIGINT NOT NULL;
+    ADD COLUMN updated_by BIGINT;
 
 ALTER TABLE user_follow
     ADD COLUMN created_by BIGINT NOT NULL,
-    ADD COLUMN updated_by BIGINT NOT NULL;
+    ADD COLUMN updated_by BIGINT;
 
 ALTER TABLE post
     ADD COLUMN created_by BIGINT NOT NULL,
-    ADD COLUMN updated_by BIGINT NOT NULL;
+    ADD COLUMN updated_by BIGINT;
 
 ALTER TABLE comment
     ADD COLUMN created_by BIGINT NOT NULL,
-    ADD COLUMN updated_by BIGINT NOT NULL;
+    ADD COLUMN updated_by BIGINT;
 
 ALTER TABLE vote
     ADD COLUMN created_by BIGINT NOT NULL,
-    ADD COLUMN updated_by BIGINT NOT NULL;
+    ADD COLUMN updated_by BIGINT;
 
 ALTER TABLE bookmark
     ADD COLUMN created_by BIGINT NOT NULL;
 
 ALTER TABLE tag
     ADD COLUMN created_by BIGINT NOT NULL,
-    ADD COLUMN updated_by BIGINT NOT NULL;
+    ADD COLUMN updated_by BIGINT;
 
 ALTER TABLE post_tag
     ADD COLUMN created_by BIGINT NOT NULL;
