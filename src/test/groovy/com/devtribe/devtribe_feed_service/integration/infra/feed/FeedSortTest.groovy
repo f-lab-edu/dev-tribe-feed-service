7fd87a3fed0ae8a50d3c721c3b74f9593eb53ad4
codongmin <codongmin@gmail.com>
2025-03-28 16:47:21 +0900

    [#20] Test: Feed 정렬 옵션에 대한 데이터 조회 테스트 케이스 추가

--- src/test/groovy/com/devtribe/devtribe_feed_service/integration/infra/feed/FeedSortTest.groovy
+++ src/test/groovy/com/devtribe/devtribe_feed_service/integration/infra/feed/FeedSortTest.groovy
@@ -7,8 +7,7 @@ import com.devtribe.devtribe_feed_service.integration.DatabaseCleaner
 import com.devtribe.devtribe_feed_service.post.application.interfaces.FeedRepository
 import com.devtribe.devtribe_feed_service.post.application.interfaces.PostRepository
 import com.devtribe.devtribe_feed_service.post.domain.FeedSortOption
-import com.devtribe.devtribe_feed_service.post.domain.Post
-import com.devtribe.devtribe_feed_service.post.domain.Publication
+import com.devtribe.devtribe_feed_service.test.utils.fixtures.post.PostFixture
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -34,30 +33,97 @@ class FeedSortTest extends AbstractIntegrationTest {
         databaseCleaner.clear()
     }
 
-    def "피드가 최신순으로 정렬되어 반환되어야 한다"() {
+    def "정렬 옵션이 최신순일 경우 피드는 최신순으로 정렬되어야 한다."() {
         given:
-        postRepository.save(
-                new Post(
-                        userId: 1L,
-                        title: "title",
-                        content: "content",
-                        thumbnail: "thumbnail",
-                        publication: Publication.PUBLIC,
-                        upvoteCount: 1,
-                        downvoteCount: 3,
-                        isDeleted: false,
-                        createdAt: LocalDateTime.now().minusMinutes(1),
-                        createdBy: 1L
-                )
-        )
+        postRepository.save(PostFixture.createPost(title: "title1", createdAt: LocalDateTime.now()))
+        postRepository.save(PostFixture.createPost(title: "title2", createdAt: LocalDateTime.now().plusMinutes(2)))
+        postRepository.save(PostFixture.createPost(title: "title3", createdAt: LocalDateTime.now().plusMinutes(3)))
+        postRepository.save(PostFixture.createPost(title: "title4", createdAt: LocalDateTime.now().plusMinutes(4)))
+        postRepository.save(PostFixture.createPost(title: "title5", createdAt: LocalDateTime.now().plusMinutes(5)))
 
-        def pagination = new CursorPagination(null, 5)
+        def expectedPostId = List.of(5, 4, 3, 2, 1)
+
+        def pagination = new CursorPagination(null, null)
         def newest = FeedSortOption.BY_NEWEST
 
         when:
         def result = feedRepository.findAllBySortOption(pagination, newest)
 
         then:
-        result.totalCount() == 1
+        result.totalCount() == expectedPostId.size()
+        result.data().eachWithIndex { post, index ->
+            assert post.id == expectedPostId[index]
+        }
     }
+
+    def "정렬 옵션이 오래된순일 경우 피드는 오래된순으로 정렬되어야 한다."() {
+        given:
+        postRepository.save(PostFixture.createPost(title: "title1", createdAt: LocalDateTime.now()))
+        postRepository.save(PostFixture.createPost(title: "title2", createdAt: LocalDateTime.now().plusMinutes(2)))
+        postRepository.save(PostFixture.createPost(title: "title3", createdAt: LocalDateTime.now().plusMinutes(3)))
+        postRepository.save(PostFixture.createPost(title: "title4", createdAt: LocalDateTime.now().plusMinutes(4)))
+        postRepository.save(PostFixture.createPost(title: "title5", createdAt: LocalDateTime.now().plusMinutes(5)))
+
+        def expectedPostId = List.of(1, 2, 3, 4, 5)
+
+        def pagination = new CursorPagination(null, null)
+        def oldest = FeedSortOption.BY_DOWNVOTE
+
+        when:
+        def result = feedRepository.findAllBySortOption(pagination, oldest)
+
+        then:
+        result.totalCount() == expectedPostId.size()
+        result.data().eachWithIndex { post, index ->
+            assert post.id == expectedPostId[index]
+        }
+    }
+
+
+    def "정렬 옵션이 추천순일 경우 피드는 오래된순으로 정렬되어야 한다."() {
+        given:
+        postRepository.save(PostFixture.createPost(title: "title1", upvoteCount: 10))
+        postRepository.save(PostFixture.createPost(title: "title2", upvoteCount: 50))
+        postRepository.save(PostFixture.createPost(title: "title3", upvoteCount: 13))
+        postRepository.save(PostFixture.createPost(title: "title4", upvoteCount: 41))
+        postRepository.save(PostFixture.createPost(title: "title5", upvoteCount: 19))
+
+        def expectedUpvoteCount = List.of(50, 41, 19, 13, 10)
+
+        def pagination = new CursorPagination(null, null)
+        def upvote = FeedSortOption.BY_UPVOTE
+
+        when:
+        def result = feedRepository.findAllBySortOption(pagination, upvote)
+
+        then:
+        result.totalCount() == 5
+        result.data().eachWithIndex { post, index ->
+            assert post.upvoteCount == expectedUpvoteCount[index]
+        }
+    }
+
+    def "정렬 옵션이 비추천순일 경우 피드는 오래된순으로 정렬되어야 한다."() {
+        given:
+        postRepository.save(PostFixture.createPost(title: "title1", downvoteCount: 10))
+        postRepository.save(PostFixture.createPost(title: "title2", downvoteCount: 50))
+        postRepository.save(PostFixture.createPost(title: "title3", downvoteCount: 13))
+        postRepository.save(PostFixture.createPost(title: "title4", downvoteCount: 41))
+        postRepository.save(PostFixture.createPost(title: "title5", downvoteCount: 19))
+
+        def expectedDownVoteCount = List.of(50, 41, 19, 13, 10)
+
+        def pagination = new CursorPagination(null, null)
+        def downvote = FeedSortOption.BY_DOWNVOTE
+
+        when:
+        def result = feedRepository.findAllBySortOption(pagination, downvote)
+
+        then:
+        result.totalCount() == 5
+        result.data().eachWithIndex { post, index ->
+            assert post.downvoteCount == expectedDownVoteCount[index]
+        }
+    }
+
 }
