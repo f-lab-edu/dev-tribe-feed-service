33af31adb06d022a4295ff919261f16e0a97deb1
codongmin <codongmin@gmail.com>
2025-04-01 16:58:39 +0900

    [#23] Refactor: PageResponse 파라미터 변경
    - 데이터와 페이지 넘버를 담도록 수정

--- src/main/java/com/devtribe/devtribe_feed_service/global/common/PageResponse.java
+++ src/main/java/com/devtribe/devtribe_feed_service/global/common/PageResponse.java
@@ -2,6 +2,6 @@ package com.devtribe.devtribe_feed_service.global.common;
 
 import java.util.List;
 
-public record PageResponse<T>(List<T> data, Long nextCursor, Long totalCount, boolean hasNextPage) {
+public record PageResponse<T>(List<T> data, int pageNo) {
 
 }
