c04f7c9487200ef01e24301b8f85adca73d17cb4
codongmin <codongmin@gmail.com>
2025-04-02 02:17:10 +0900

    [#23] Refactor: 피드 페이징 방식 변경에 따른 FeedRepositoryImpl 변경
    - 커서 기반 페이지네이션에서 offset 기반 페이지네이션으로 변경

--- src/main/java/com/devtribe/devtribe_feed_service/post/repository/FeedRepositoryImpl.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/repository/FeedRepositoryImpl.java
@@ -2,14 +2,14 @@ package com.devtribe.devtribe_feed_service.post.repository;
 
 import static com.devtribe.devtribe_feed_service.post.domain.QPost.post;
 
-import com.devtribe.devtribe_feed_service.global.common.CursorPagination;
 import com.devtribe.devtribe_feed_service.global.common.PageResponse;
+import com.devtribe.devtribe_feed_service.post.application.dtos.FeedSearchRequest;
 import com.devtribe.devtribe_feed_service.post.application.interfaces.FeedRepository;
+import com.devtribe.devtribe_feed_service.post.domain.FeedFilterOption;
 import com.devtribe.devtribe_feed_service.post.domain.FeedSortOption;
 import com.devtribe.devtribe_feed_service.post.domain.Post;
 import com.devtribe.devtribe_feed_service.post.repository.query.SortQuery;
 import com.devtribe.devtribe_feed_service.post.repository.query.SortQueryFactory;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import org.springframework.stereotype.Repository;
@@ -26,46 +26,24 @@ public class FeedRepositoryImpl implements FeedRepository {
     }
 
     @Override
-    public PageResponse<Post> findAllBySortOption(
-        CursorPagination cursorPagination,
-        FeedSortOption feedSortOption
-    ) {
-        Long cursorId = cursorPagination.cursorId();
-        Integer pageSize = cursorPagination.pageSize();
+    public PageResponse<Post> findFeedsByFilterAndSortOption(FeedSearchRequest feedSearchRequest) {
+        FeedSortOption feedSortOption = feedSearchRequest.getFeedSortOption();
+        FeedFilterOption feedFilterOption = feedSearchRequest.getFeedFilterOption();
+        int offset = feedSearchRequest.getOffset();
+        int size = feedSearchRequest.getSize();
 
         SortQuery sortQuery = sortQueryFactory.getSortQuery(feedSortOption);
-        Post cursorPost = getCursorPost(cursorId);
-        BooleanExpression whereCondition =
-            cursorPost != null ? sortQuery.getWhereCondition(cursorPost) : null;
 
         List<Post> queryResult = queryFactory
             .selectFrom(post)
-            .where(whereCondition)
+            .where(
+                /// feedFilterOption 추가
+            )
             .orderBy(sortQuery.getOrderBy())
-            .limit(pageSize + 1)
+            .offset(offset)
+            .limit((long) offset * size)
             .fetch();
 
-        return getPageResponse(queryResult, pageSize);
-    }
-
-    private Post getCursorPost(Long cursorId) {
-        if (cursorId == null) {
-            return null;
-        }
-        return queryFactory.selectFrom(post)
-            .where(post.id.eq(cursorId))
-            .fetchOne();
-    }
-
-    private PageResponse<Post> getPageResponse(List<Post> queryResult, Integer pageSize) {
-        if (queryResult.isEmpty()) {
-            return new PageResponse<>(List.of(), null, 0L, false);
-        }
-
-        boolean hasMore = queryResult.size() > pageSize;
-        List<Post> data = queryResult.subList(0, Math.min(queryResult.size(), pageSize));
-        Long nextCursor = hasMore ? queryResult.get(pageSize).getId() : null;
-
-        return new PageResponse<>(data, nextCursor, (long) data.size(), hasMore);
+        return new PageResponse<>(queryResult, offset);
     }
 }
