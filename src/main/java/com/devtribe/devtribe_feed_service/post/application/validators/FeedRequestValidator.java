941b4d1c40caf8bd818506fabcac9506c49d10ba
codongmin <codongmin@gmail.com>
2025-04-02 02:10:58 +0900

    [#23] Refactor: pageSize 검증 로직 변경

--- src/main/java/com/devtribe/devtribe_feed_service/post/application/validators/FeedRequestValidator.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/application/validators/FeedRequestValidator.java
@@ -1,6 +1,5 @@
 package com.devtribe.devtribe_feed_service.post.application.validators;
 
-import com.devtribe.devtribe_feed_service.global.common.CursorPagination;
 import com.devtribe.devtribe_feed_service.post.domain.FeedSortOption;
 import com.google.common.base.Preconditions;
 import java.util.Arrays;
@@ -9,10 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class FeedRequestValidator {
 
-    public void validateCursorPagination(CursorPagination cursorPagination) {
-        Preconditions.checkArgument(cursorPagination != null, "유효하지 않은 요청 값입니다.");
-        Preconditions.checkArgument(cursorPagination.pageSize() != null, "페이지 수는 null일 수 없습니다.");
-        Preconditions.checkArgument(cursorPagination.isPageSizeInRange(), "요청한 페이지 수의 범위가 올바르지 않습니다.");
+    private static final int MAX_PAGE_SIZE = 30;
+
+    public void validatePagination(int size) {
+        Preconditions.checkArgument(size < MAX_PAGE_SIZE, "요청한 페이지 수의 범위가 올바르지 않습니다.");
     }
 
     public void validateSortOption(FeedSortOption sort) {
