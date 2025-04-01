5401b38da6c9041558dca07862c4d610d83d2685
codongmin <codongmin@gmail.com>
2025-04-02 02:12:07 +0900

    [#23] Refactor: FeedService 피드 검색 파라미터 변경

--- src/main/java/com/devtribe/devtribe_feed_service/post/application/FeedService.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/application/FeedService.java
@@ -1,11 +1,10 @@
 package com.devtribe.devtribe_feed_service.post.application;
 
-import com.devtribe.devtribe_feed_service.global.common.CursorPagination;
 import com.devtribe.devtribe_feed_service.global.common.PageResponse;
+import com.devtribe.devtribe_feed_service.post.application.dtos.FeedSearchRequest;
 import com.devtribe.devtribe_feed_service.post.application.dtos.PostResponse;
 import com.devtribe.devtribe_feed_service.post.application.interfaces.FeedRepository;
 import com.devtribe.devtribe_feed_service.post.application.validators.FeedRequestValidator;
-import com.devtribe.devtribe_feed_service.post.domain.FeedSortOption;
 import com.devtribe.devtribe_feed_service.post.domain.Post;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -23,9 +22,9 @@ public class FeedService {
     }
 
     @Transactional(readOnly = true)
-    public PageResponse<PostResponse> findFeedBySearchOption(CursorPagination cursorPagination, FeedSortOption sort) {
-        feedRequestValidator.validateCursorPagination(cursorPagination);
-        feedRequestValidator.validateSortOption(sort);
+    public PageResponse<PostResponse> findFeedBySearchOption(FeedSearchRequest feedSearchRequest) {
+        feedRequestValidator.validatePagination(feedSearchRequest.getSize());
+        feedRequestValidator.validateSortOption(feedSearchRequest.getFeedSortOption());
 
         PageResponse<Post> postPageResponse = feedRepository.findAllBySortOption(cursorPagination, sort);
 
