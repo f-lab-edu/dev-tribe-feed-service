ce011751ac714c8d4a5b19a36c963b4231a2d8ef
codongmin <codongmin@gmail.com>
2025-04-03 14:12:55 +0900

    [#20] Refactor: 테스트 설정 레포지토리 빈 타입을 인터페이스로 변경

--- src/test/groovy/com/devtribe/devtribe_feed_service/integration/DataTestConfig.java
+++ src/test/groovy/com/devtribe/devtribe_feed_service/integration/DataTestConfig.java
@@ -1,10 +1,13 @@
 package com.devtribe.devtribe_feed_service.integration;
 
+import com.devtribe.devtribe_feed_service.post.application.interfaces.FeedRepository;
+import com.devtribe.devtribe_feed_service.post.application.interfaces.PostRepository;
 import com.devtribe.devtribe_feed_service.post.repository.FeedRepositoryImpl;
 import com.devtribe.devtribe_feed_service.post.repository.PostRepositoryImpl;
 import com.devtribe.devtribe_feed_service.post.repository.jpa.JpaPostRepository;
 import com.devtribe.devtribe_feed_service.post.repository.query.QueryFeedBySortOption;
 import com.devtribe.devtribe_feed_service.post.repository.query.SortQueryFactory;
+import com.devtribe.devtribe_feed_service.user.application.interfaces.UserRepository;
 import com.devtribe.devtribe_feed_service.user.repository.UserRepositoryImpl;
 import com.devtribe.devtribe_feed_service.user.repository.jpa.JpaUserRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -21,17 +24,17 @@ public class DataTestConfig {
     }
 
     @Bean
-    public UserRepositoryImpl userRepository(JpaUserRepository jpaUserRepository) {
+    public UserRepository userRepository(JpaUserRepository jpaUserRepository) {
         return new UserRepositoryImpl(jpaUserRepository);
     }
 
     @Bean
-    public PostRepositoryImpl postRepository(JpaPostRepository jpaPostRepository) {
+    public PostRepository postRepository(JpaPostRepository jpaPostRepository) {
         return new PostRepositoryImpl(jpaPostRepository);
     }
 
     @Bean
-    public FeedRepositoryImpl feedRepository(QueryFeedBySortOption queryFeedBySortOption) {
+    public FeedRepository feedRepository(QueryFeedBySortOption queryFeedBySortOption) {
         return new FeedRepositoryImpl(queryFeedBySortOption);
     }
 
