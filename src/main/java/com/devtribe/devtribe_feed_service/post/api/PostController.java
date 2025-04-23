c7a68989bcfbf97e5b908b2971bd64c73a84b799
codongmin <codongmin@gmail.com>
2025-04-23 18:23:44 +0900

    [#30] Feat: 게시글 추천/비추천 api 추가

--- src/main/java/com/devtribe/devtribe_feed_service/post/api/PostController.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/api/PostController.java
@@ -1,11 +1,18 @@
 package com.devtribe.devtribe_feed_service.post.api;
 
+import static com.devtribe.devtribe_feed_service.post.application.dtos.VoteRequest.downvotePostRequest;
+import static com.devtribe.devtribe_feed_service.post.application.dtos.VoteRequest.upvotePostRequest;
+
+import com.devtribe.devtribe_feed_service.global.config.security.authentication.CustomUserDetail;
 import com.devtribe.devtribe_feed_service.post.application.PostService;
+import com.devtribe.devtribe_feed_service.post.application.VoteService;
 import com.devtribe.devtribe_feed_service.post.application.dtos.CreatePostRequest;
 import com.devtribe.devtribe_feed_service.post.application.dtos.CreatePostResponse;
+import com.devtribe.devtribe_feed_service.post.application.dtos.PostVoteResponse;
 import com.devtribe.devtribe_feed_service.post.application.dtos.UpdatePostRequest;
 import com.devtribe.devtribe_feed_service.post.application.dtos.UpdatePostResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+    private final VoteService voteService;
 
-    public PostController(PostService postService) {
+    public PostController(PostService postService, VoteService voteService) {
         this.postService = postService;
+        this.voteService = voteService;
     }
 
     @PostMapping
@@ -43,5 +52,25 @@ public class PostController {
     public ResponseEntity<Void> deletePost(@PathVariable("id") Long id) {
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/upvote")
+    public ResponseEntity<PostVoteResponse> upvote(
+        @PathVariable("id") Long postId,
+        @AuthenticationPrincipal CustomUserDetail userDetail
+    ) {
+        Long userId = userDetail.getUserId();
+        PostVoteResponse responseBody = voteService.vote(postId, upvotePostRequest(userId));
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @PostMapping("/{id}/downvote")
+    public ResponseEntity<PostVoteResponse> downvote(
+        @PathVariable("id") Long postId,
+        @AuthenticationPrincipal CustomUserDetail userDetail
+    ) {
+        Long userId = userDetail.getUserId();
+        PostVoteResponse responseBody = voteService.vote(postId, downvotePostRequest(userId));
+        return ResponseEntity.ok(responseBody);
     }
 }
