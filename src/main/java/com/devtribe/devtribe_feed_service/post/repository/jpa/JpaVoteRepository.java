6634c72737c2c376a84fb2bc55a806beb1c5fd3c
codongmin <codongmin@gmail.com>
2025-05-08 20:18:29 +0900

    [#30] Feat: 비관적 락 추가

--- src/main/java/com/devtribe/devtribe_feed_service/post/repository/jpa/JpaVoteRepository.java
+++ src/main/java/com/devtribe/devtribe_feed_service/post/repository/jpa/JpaVoteRepository.java
@@ -1,11 +1,14 @@
 package com.devtribe.devtribe_feed_service.post.repository.jpa;
 
 import com.devtribe.devtribe_feed_service.post.domain.vote.PostVote;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface JpaVoteRepository extends JpaRepository<PostVote, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_READ)
     Optional<PostVote> findByUserIdAndPostId(Long userId, Long postId);
 
 }
