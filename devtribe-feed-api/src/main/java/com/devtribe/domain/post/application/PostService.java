4b09106c466465f872d363555b2fad3000c78e52
codongmin <codongmin@gmail.com>
2025-05-28 21:38:27 +0900

    [#38] Feat: 게시글 태그 추가 기능
    - 전체 삭제 후 삽입하는 간단한 방식으로 구현.
    - 이미 태그가 달려있는 경우에도 모두 삭제 후 새로 생성하기 때문에 최적화 여지있음. 구조 변경 후 개선 예정

--- devtribe-feed-api/src/main/java/com/devtribe/domain/post/application/PostService.java
+++ devtribe-feed-api/src/main/java/com/devtribe/domain/post/application/PostService.java
@@ -6,10 +6,15 @@ import com.devtribe.domain.post.application.dtos.UpdatePostRequest;
 import com.devtribe.domain.post.application.dtos.UpdatePostResponse;
 import com.devtribe.domain.post.application.validators.PostRequestValidator;
 import com.devtribe.domain.post.dao.PostJpaRepository;
+import com.devtribe.domain.post.dao.PostTagRepository;
 import com.devtribe.domain.post.entity.Post;
+import com.devtribe.domain.post.entity.PostTag;
+import com.devtribe.domain.tag.dao.TagRepository;
+import com.devtribe.domain.tag.entity.Tag;
 import com.devtribe.domain.user.application.UserService;
 import com.devtribe.domain.user.entity.User;
 import com.google.common.base.Preconditions;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,15 +23,21 @@ public class PostService {
 
     private final PostRequestValidator postRequestValidator;
     private final PostJpaRepository postRepository;
+    private final PostTagRepository postTagRepository;
+    private final TagRepository tagRepository;
     private final UserService userService;
 
     public PostService(
         PostRequestValidator postRequestValidator,
         PostJpaRepository postRepository,
+        PostTagRepository postTagRepository,
+        TagRepository tagRepository,
         UserService userService
     ) {
         this.postRequestValidator = postRequestValidator;
         this.postRepository = postRepository;
+        this.postTagRepository = postTagRepository;
+        this.tagRepository = tagRepository;
         this.userService = userService;
     }
 
@@ -43,6 +54,8 @@ public class PostService {
 
         User findUser = userService.getUser(request.authorId());
         Post savedPost = postRepository.save(request.toEntity(findUser));
+
+        updatePostTag(savedPost.getId(), request.tags());
         return new CreatePostResponse(savedPost.getId());
     }
 
@@ -56,6 +69,7 @@ public class PostService {
         validateAuthor(findPost, requestAuthor);
 
         findPost.updatePostDetail(request.title(), request.content(), request.thumbnail(), request.publication());
+        updatePostTag(findPost.getId(), request.tags());
         return UpdatePostResponse.from(findPost);
     }
 
@@ -63,6 +77,25 @@ public class PostService {
     public void deletePost(Long postId) {
         Post findPost = getPost(postId);
         findPost.deletePost();
+    }
+
+    /**
+     * TODO: 최적화 필요  간단 구현, 전체 delete 후 전체 insert 방식
+     *
+     */
+    @Transactional
+    public void updatePostTag(Long postId, List<Long> tagList) {
+        postRequestValidator.validateTagListSize(tagList);
+
+        List<Tag> existingTag = tagRepository.findAllById(tagList);
+
+        postTagRepository.deleteAllByPostId(postId);
+        postTagRepository.flush();
+
+        List<PostTag> newPostTags = existingTag.stream()
+            .map(tag -> new PostTag(postId, tag.getId()))
+            .toList();
+        postTagRepository.saveAll(newPostTags);
     }
 
     private void validateAuthor(Post findPost, User findAuthor) {
