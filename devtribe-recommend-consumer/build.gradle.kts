03a00c0f769439a228a3ae29cad7e8548a224fc2
codongmin <codongmin@gmail.com>
2025-06-04 01:50:50 +0900

    [#47] Chore: spring cloud stream 의존성 추가

--- devtribe-recommend-consumer/build.gradle.kts
+++ devtribe-recommend-consumer/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation(project(mapOf("path" to ":devtribe-recommend-core")))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
 }
