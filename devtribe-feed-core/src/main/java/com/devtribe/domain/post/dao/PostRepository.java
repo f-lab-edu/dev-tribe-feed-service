a32b75aec01817959b0ebd3700baae2ff7fd5fde
codongmin <codongmin@gmail.com>
2025-07-04 22:16:29 +0900

    [#29] Refactor: PostRepository에 PostSearchRepository 커스텀 인터페이스 병합

--- devtribe-feed-core/src/main/java/com/devtribe/domain/post/dao/PostRepository.java
+++ devtribe-feed-core/src/main/java/com/devtribe/domain/post/dao/PostRepository.java
@@ -3,6 +3,6 @@ package com.devtribe.domain.post.dao;
 import com.devtribe.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostSearchRepository {
 
 }
