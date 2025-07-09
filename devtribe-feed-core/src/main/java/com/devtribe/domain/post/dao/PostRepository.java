3350aa4a2f2599268a65264013d47995b1636805
codongmin <codongmin@gmail.com>
2025-07-09 14:51:36 +0900

    [#29] Refactor: 도메인 레이어의 게시글 조회와 검색 책임을 분리

--- devtribe-feed-core/src/main/java/com/devtribe/domain/post/dao/PostRepository.java
+++ devtribe-feed-core/src/main/java/com/devtribe/domain/post/dao/PostRepository.java
@@ -3,6 +3,6 @@ package com.devtribe.domain.post.dao;
 import com.devtribe.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostSearchRepository {
+public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository, PostSearchRepository {
 
 }
